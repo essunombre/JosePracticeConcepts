49edb227a6828f49bb73319ac358072ccb20a272
Jose Albancando Robles <jalbancando@premier.pr>
2025-06-19 22:10:37 -0500

    :construction_worker: Starting Password project
    Password branch creation

---
+++ w2/password_project/passwords.py
@@ -0,0 +1,17 @@
+def main():
+  toppaswords = ('/toppaswords')
+  wordlist = ('/wordlist')
+
+  def word_in_file(word, filename, case_sensitive):
+    pass
+  
+  def word_has_character(word, character_list):
+    pass
+  
+  def word_complexity(word):
+    pass
+  
+  def word_complexity(password, min_length, strong_length):
+    pass
+  
+main()
